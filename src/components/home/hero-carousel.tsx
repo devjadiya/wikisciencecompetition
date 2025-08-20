7ff11373af0c43a6c40ea3fd79add261564f7ed4
Dev Jadiya <devjadiya15@gmail.com>
2025-08-20 14:39:01 +0000

    updated

--- src/components/home/hero-carousel.tsx
+++ src/components/home/hero-carousel.tsx
@@ -10,41 +10,49 @@ const slides = [
     id: 1,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/IA_ARPAN_BASU_CHOWDHURY_Foldscope_-_A_revolutionary_Microscope.jpg' },
     alt: 'Foldscope - A revolutionary Microscope by Arpan Basu Chowdhury',
+    caption: 'Foldscope - A revolutionary Microscope by Arpan Basu Chowdhury, CC BY-SA 4.0',
   },
   {
     id: 2,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Yellow_orange-tip_male_Ixias_pyrene.jpg' },
     alt: 'Yellow orange-tip male Ixias pyrene',
+    caption: 'Yellow orange-tip male by Jeevan Jose, CC BY-SA 4.0',
   },
   {
     id: 3,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Yellow_orange-tip_female_Ixias_pyrene.jpg' },
     alt: 'Yellow orange-tip female Ixias pyrene',
+    caption: 'Yellow orange-tip female by Jeevan Jose, CC BY-SA 4.0',
   },
   {
     id: 4,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/A_Coot%27s_Scoot.jpg' },
     alt: 'A Coot\'s Scoot',
+    caption: 'A Coot\'s Scoot by Ayonija Ghosh, CC BY-SA 4.0',
   },
   {
     id: 5,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Sarus_Crane_Duet.jpg' },
     alt: 'Sarus Crane Duet',
+    caption: 'Sarus Crane Duet by A J Ganaie, CC BY-SA 4.0',
   },
   {
     id: 6,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Greater_Crimson_Glider_%28Urothemis_signata%29.jpg' },
     alt: 'Greater Crimson Glider (Urothemis signata)',
+    caption: 'Greater Crimson Glider by Sayan Hazra, CC BY-SA 4.0',
   },
   {
     id: 7,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/d/d8/Indian_Gharial.jpg' },
     alt: 'Indian Gharial',
+    caption: 'Indian Gharial by A J Ganaie, CC BY-SA 4.0',
   },
   {
     id: 8,
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/3/30/PSLV_C47_Cartosat-3_lifting_off_from_Second_Launch_Pad_003.jpg' },
     alt: 'PSLV C47 Cartosat-3 lifting off from Second Launch Pad',
+    caption: 'PSLV C47 Cartosat-3 lift off by ISRO, CC BY-SA 4.0',
   },
 ];
 
@@ -105,6 +113,9 @@ export default function HeroCarousel() {
             priority={slideIndex === 0}
           />
            <div className="absolute inset-0 bg-gradient-to-t from-primary/30 via-primary/10 to-transparent" />
+           <div className="absolute bottom-4 right-4 p-2 bg-black/50 text-white rounded-md text-xs text-right">
+             {slides[slideIndex].caption}
+           </div>
         </motion.div>
       </AnimatePresence>
 
