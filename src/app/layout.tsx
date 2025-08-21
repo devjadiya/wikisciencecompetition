6e7d0b67ceed1600d9714ad8201000fb06f24f19
Dev Jadiya <devjadiya15@gmail.com>
2025-08-21 06:52:49 +0000

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Navbar from '@/components/layout/navbar';
 import Footer from '@/components/layout/footer';
 import FaqChatbot from '@/components/faq-chatbot';
 import { LanguageProvider } from '@/context/language-context';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'Wiki Science Competition 2025 - India',
@@ -59,12 +60,23 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
       <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N9KGHGR6');
+          `}
+        </Script>
         <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/e/ee/Logo_for_Wiki_Science_Competition_India.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600;700&family=Source+Sans+Pro:wght@300;400;500&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('font-body antialiased bg-background text-foreground min-h-screen flex flex-col')}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9KGHGR6"
+            height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
         <LanguageProvider>
           <Navbar />
           <main className="flex-grow">{children}</main>
