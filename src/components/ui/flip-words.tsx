72678e019125b26c521de9b205b35aebaa536365
Dev Jadiya <devjadiya15@gmail.com>
2025-08-27 05:27:26 +0000

    updated

--- src/components/ui/flip-words.tsx
+++ src/components/ui/flip-words.tsx
@@ -30,51 +30,51 @@ export const FlipWords = ({
   }, [isAnimating, duration, startAnimation]);
 
   return (
-      <AnimatePresence
-        onExitComplete={() => {
-          setIsAnimating(false);
+    <AnimatePresence
+      onExitComplete={() => {
+        setIsAnimating(false);
+      }}
+    >
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: 10,
         }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          type: "spring",
+          stiffness: 100,
+          damping: 10,
+        }}
+        exit={{
+          opacity: 0,
+          y: -10,
+          filter: "blur(8px)",
+        }}
+        className={cn(
+          "relative z-10 inline-block px-2 pb-1 bg-gradient-to-r from-indigo-300 to-purple-300 text-black dark:from-indigo-500 dark:to-purple-500 rounded-lg",
+          className
+        )}
+        key={currentWord}
       >
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: 10,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            type: "spring",
-            stiffness: 100,
-            damping: 10,
-          }}
-          exit={{
-            opacity: 0,
-            y: -10,
-            filter: "blur(8px)",
-          }}
-          className={cn(
-            "relative z-10 inline-block whitespace-nowrap rounded-lg bg-gradient-to-r from-indigo-300 to-purple-300 px-2 pb-1 text-black dark:from-indigo-500 dark:to-purple-500",
-            className
-          )}
-          key={currentWord}
-        >
-          {currentWord.split("").map((letter, index) => (
-            <motion.span
-              key={currentWord + index}
-              initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
-              animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
-              transition={{
-                delay: index * 0.08,
-                duration: 0.4,
-              }}
-              className="inline-block"
-            >
-              {letter}
-            </motion.span>
-          ))}
-        </motion.div>
-      </AnimatePresence>
+        {currentWord.split("").map((letter, index) => (
+          <motion.span
+            key={currentWord + index}
+            initial={{ opacity: 0, y: 10, filter: "blur(8px)" }}
+            animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
+            transition={{
+              delay: index * 0.08,
+              duration: 0.4,
+            }}
+            className="inline-block"
+          >
+            {letter}
+          </motion.span>
+        ))}
+      </motion.div>
+    </AnimatePresence>
   );
 };
