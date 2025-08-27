99e03c2b707d31afea2bb2e995b68e8097079f8b
Dev Jadiya <devjadiya15@gmail.com>
2025-08-27 08:23:34 +0000

    updated

--- src/components/home/hero-carousel.tsx
+++ src/components/home/hero-carousel.tsx
@@ -9,39 +9,39 @@ import { ChevronLeft, ChevronRight } from 'lucide-react';
 const slides = [
   {
     id: 1,
-    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/IA_ARPAN_BASU_CHOWDHURY_Foldscope_-_A_revolutionary_Microscope.jpg' },
-    alt: 'Foldscope - A revolutionary Microscope by Arpan Basu Chowdhury',
-    caption: 'Foldscope - A revolutionary Microscope by Arpan Basu Chowdhury, CC BY-SA 4.0',
+    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/f/f5/FMN_Lab_team_%282%29.jpg' },
+    alt: 'FMN Lab team by FMNLab',
+    caption: 'FMN Lab team by FMNLab, CC BY-SA 4.0',
   },
   {
     id: 2,
-    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Yellow_orange-tip_male_Ixias_pyrene.jpg' },
-    alt: 'Yellow orange-tip male Ixias pyrene',
-    caption: 'Yellow orange-tip male by Jeevan Jose, CC BY-SA 4.0',
+    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/0/07/Ascaris_female_200x_section.jpg' },
+    alt: 'Ascaris female 200x section',
+    caption: 'Ascaris female 200x section by Massimo brizzi, CC BY-SA 4.0',
   },
   {
     id: 3,
-    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Yellow_orange-tip_female_Ixias_pyrene.jpg' },
-    alt: 'Yellow orange-tip female Ixias pyrene',
-    caption: 'Yellow orange-tip female by Jeevan Jose, CC BY-SA 4.0',
+    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/1/1f/Wismut_Kristall_und_1cm3_Wuerfel.jpg' },
+    alt: 'Wismut Kristall und 1cm3 Wuerfel',
+    caption: 'Wismut Kristall und 1cm3 Wuerfel by Alchemist-hp, CC BY-SA 3.0',
   },
   {
     id: 4,
-    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/A_Coot%27s_Scoot.jpg' },
-    alt: 'A Coot\'s Scoot',
-    caption: 'A Coot\'s Scoot by Ayonija Ghosh, CC BY-SA 4.0',
+    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/9/96/Macrocranion_tupaiodon_01.jpg' },
+    alt: 'Macrocranion tupaiodon',
+    caption: 'Macrocranion tupaiodon by Llez, CC BY-SA 3.0',
   },
   {
     id: 5,
-    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Sarus_Crane_Duet.jpg' },
-    alt: 'Sarus Crane Duet',
-    caption: 'Sarus Crane Duet by A J Ganaie, CC BY-SA 4.0',
+    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Instrumente_in_der_Maschinenhalle_der_Zeche_Zollern.jpg' },
+    alt: 'Instrumente in der Maschinenhalle der Zeche Zollern',
+    caption: 'Instrumente in der Maschinenhalle der Zeche Zollern by GZagatta, CC BY-SA 4.0',
   },
   {
     id: 6,
-    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Greater_Crimson_Glider_%28Urothemis_signata%29.jpg' },
-    alt: 'Greater Crimson Glider (Urothemis signata)',
-    caption: 'Greater Crimson Glider by Sayan Hazra, CC BY-SA 4.0',
+    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Soldering_a_0805.jpg' },
+    alt: 'Soldering a 0805',
+    caption: 'Soldering a 0805 by Aisart~commonswiki, CC BY-SA 3.0',
   },
   {
     id: 7,
@@ -54,7 +54,56 @@ const slides = [
     image: { src: 'https://upload.wikimedia.org/wikipedia/commons/3/30/PSLV_C47_Cartosat-3_lifting_off_from_Second_Launch_Pad_003.jpg' },
     alt: 'PSLV C47 Cartosat-3 lifting off from Second Launch Pad',
     caption: 'PSLV C47 Cartosat-3 lift off by ISRO, CC BY-SA 4.0',
-  },
+  }
+  // ,{
+  //   id: 1,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/f/f0/IA_ARPAN_BASU_CHOWDHURY_Foldscope_-_A_revolutionary_Microscope.jpg' },
+  //   alt: 'Foldscope - A revolutionary Microscope by Arpan Basu Chowdhury',
+  //   caption: 'Foldscope - A revolutionary Microscope by Arpan Basu Chowdhury, CC BY-SA 4.0',
+  // },
+  // {
+  //   id: 2,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/2/2b/Yellow_orange-tip_male_Ixias_pyrene.jpg' },
+  //   alt: 'Yellow orange-tip male Ixias pyrene',
+  //   caption: 'Yellow orange-tip male by Jeevan Jose, CC BY-SA 4.0',
+  // },
+  // {
+  //   id: 3,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/b/b1/Yellow_orange-tip_female_Ixias_pyrene.jpg' },
+  //   alt: 'Yellow orange-tip female Ixias pyrene',
+  //   caption: 'Yellow orange-tip female by Jeevan Jose, CC BY-SA 4.0',
+  // },
+  // {
+  //   id: 4,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/e/e5/A_Coot%27s_Scoot.jpg' },
+  //   alt: 'A Coot\'s Scoot',
+  //   caption: 'A Coot\'s Scoot by Ayonija Ghosh, CC BY-SA 4.0',
+  // },
+  // {
+  //   id: 5,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Sarus_Crane_Duet.jpg' },
+  //   alt: 'Sarus Crane Duet',
+  //   caption: 'Sarus Crane Duet by A J Ganaie, CC BY-SA 4.0',
+  // },
+  // {
+  //   id: 6,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Greater_Crimson_Glider_%28Urothemis_signata%29.jpg' },
+  //   alt: 'Greater Crimson Glider (Urothemis signata)',
+  //   caption: 'Greater Crimson Glider by Sayan Hazra, CC BY-SA 4.0',
+  // },
+  // {
+  //   id: 7,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/d/d8/Indian_Gharial.jpg' },
+  //   alt: 'Indian Gharial',
+  //   caption: 'Indian Gharial by A J Ganaie, CC BY-SA 4.0',
+  // },
+  // {
+  //   id: 8,
+  //   image: { src: 'https://upload.wikimedia.org/wikipedia/commons/3/30/PSLV_C47_Cartosat-3_lifting_off_from_Second_Launch_Pad_003.jpg' },
+  //   alt: 'PSLV C47 Cartosat-3 lifting off from Second Launch Pad',
+  //   caption: 'PSLV C47 Cartosat-3 lift off by ISRO, CC BY-SA 4.0',
+  // }
+  
 ];
 
 const variants = {
