d91bb16ce3ddba1836d8faf97b0d203afb8b9ed1
Dev Jadiya <devjadiya15@gmail.com>
2025-09-08 16:52:43 +0530

    updated

--- src/components/home/hero-carousel.tsx
+++ src/components/home/hero-carousel.tsx
@@ -51,9 +51,9 @@ const slides = [
   },
   {
     id: 8,
-    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/Gyri_of_lateral_cortex.png' },
-    alt: 'Gyri of lateral cortex',
-    caption: 'Gyri of lateral cortex by Iztwoz, CC BY-SA 4.0',
+    image: { src: 'https://upload.wikimedia.org/wikipedia/commons/e/ea/%D0%92%D0%BD%D0%B5%D1%88%D0%BD%D0%B8%D0%B9_%D1%81%D0%BB%D0%BE%D0%B9_%D0%BC%D0%B5%D0%B4%D0%B8%D1%86%D0%B8%D0%BD%D1%81%D0%BA%D0%BE%D0%B9_%D0%BC%D0%B0%D1%81%D0%BA%D0%B8_%28%D0%BF%D0%BE%D0%BB%D1%8F%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F%29.tif' },
+    alt: 'Microscopic image of the outer layer of a surgical mask',
+    caption: 'Microscopic outer layer of a surgical mask by Alexander Klepnev, CC BY-SA 4.0',
   }
   // ,{
   //   id: 1,
